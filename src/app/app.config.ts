95c44d202952090ce29697f7150381c0baff9c31
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 15:58:00 -0400

    feat: add app initializer for translation service in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,22 @@
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import {
+    ApplicationConfig,
+    inject,
+    provideAppInitializer,
+    provideBrowserGlobalErrorListeners,
+} from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { authInterceptor } from "./core/interceptors/auth.interceptor";
 import { errorInterceptor } from "./core/interceptors/error.interceptor";
+import { TranslationService } from "./core/services/translation.service";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes, withComponentInputBinding()),
         provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+        provideAppInitializer(() => inject(TranslationService).load()),
     ],
 };
