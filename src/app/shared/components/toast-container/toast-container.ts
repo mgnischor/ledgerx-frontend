9cee94d017f2ef00b3510555a37f673d22f19c20
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 16:01:20 -0400

    feat: add TranslatePipe to toast container for improved accessibility

--- src/app/shared/components/toast-container/toast-container.ts
+++ src/app/shared/components/toast-container/toast-container.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from "@angular/core";
 import { ToastService } from "../../../core/services/toast.service";
+import { TranslatePipe } from "../../pipes/translate.pipe";
 
 @Component({
     selector: "app-toast-container",
+    imports: [TranslatePipe],
     templateUrl: "./toast-container.html",
     styleUrl: "./toast-container.scss",
 })
