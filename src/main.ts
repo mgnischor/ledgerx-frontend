d9188a8dc873d4bd69e6d86d9ae60f2fefc4a89e
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 15:57:55 -0400

    feat: register locale data for English, Spanish, and Portuguese

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import { registerLocaleData } from "@angular/common";
+import localeEn from "@angular/common/locales/en";
+import localeEs from "@angular/common/locales/es";
+import localePt from "@angular/common/locales/pt";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { App } from "./app/app";
+
+registerLocaleData(localeEn, "en-US");
+registerLocaleData(localePt, "pt-BR");
+registerLocaleData(localeEs, "es-ES");
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
