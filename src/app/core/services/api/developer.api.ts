8bfa288be51e2cd4d1284582c636d408380fa2e4
Miguel Nischor <miguel@nischor.com.br>
2026-07-26 22:00:53 -0400

    feat: implement DeveloperApi service for fetching developer information with response headers

---
+++ src/app/core/services/api/developer.api.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../../../environments/environment";
+import { DeveloperInfoDto } from "../../models/developer.model";
+
+@Injectable({ providedIn: "root" })
+export class DeveloperApi {
+    constructor(private readonly http: HttpClient) {}
+
+    /** Requests the full response (not just the body) to also surface the X-Debug-* trace headers. */
+    info(): Observable<HttpResponse<DeveloperInfoDto>> {
+        return this.http.get<DeveloperInfoDto>(`${environment.apiUrl}/developer`, { observe: "response" });
+    }
+}
