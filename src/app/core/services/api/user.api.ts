4e02eb8063a4877728ba364061abab17ea0274d1
Miguel Nischor <miguel@nischor.com.br>
2026-08-08 09:05:54 -0400

    feat: add documentation comments for methods in UserApi

--- src/app/core/services/api/user.api.ts
+++ src/app/core/services/api/user.api.ts
@@ -4,20 +4,41 @@ import { Observable } from "rxjs";
 import { environment } from "../../../../environments/environment";
 import { CreateUserRequest, GrantRoleRequest, UserDto } from "../../models/identity.model";
 
+/** HTTP client for the platform's user-management endpoints. */
 @Injectable({ providedIn: "root" })
 export class UserApi {
     private readonly baseUrl = `${environment.apiUrl}/users`;
 
+    /** @param http the Angular HTTP client used to issue requests */
     constructor(private readonly http: HttpClient) {}
 
+    /**
+     * Registers a new user.
+     *
+     * @param request the user data to register
+     * @returns an observable emitting the created user
+     */
     register(request: CreateUserRequest): Observable<UserDto> {
         return this.http.post<UserDto>(this.baseUrl, request);
     }
 
+    /**
+     * Grants a role to a user; idempotent on the backend.
+     *
+     * @param userId  the user's id
+     * @param request the role to grant
+     * @returns an observable emitting the updated user
+     */
     grantRole(userId: string, request: GrantRoleRequest): Observable<UserDto> {
         return this.http.patch<UserDto>(`${this.baseUrl}/${userId}/roles`, request);
     }
 
+    /**
+     * Deactivates a user; idempotent on the backend.
+     *
+     * @param userId the user's id
+     * @returns an observable emitting the deactivated user
+     */
     deactivate(userId: string): Observable<UserDto> {
         return this.http.patch<UserDto>(`${this.baseUrl}/${userId}/deactivate`, {});
     }
